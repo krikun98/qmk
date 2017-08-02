ba9d9ceb486840e026feabf423e4b647f13b8026
joric <joric@users.noreply.github.com>
2017-08-02 09:03:42 +0500

    updated mitosis-qwerty (right alt replaced to left alt for language switching)

--- keyboards/mitosis/keymaps/qwerty/keymap.c
+++ keyboards/mitosis/keymaps/qwerty/keymap.c
@@ -9,9 +9,9 @@
 // entirely and just use numbers.
 enum mitosis_layers
 {
-	_BL,
-	_LOWER,
-	_RAISE
+  _BL,
+  _LOWER,
+  _RAISE
 };
 
 enum mitosis_keycodes 
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
   {KC_A,    KC_S,    KC_D,    KC_F,     KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
   {KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,           KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH },
-  {XXXXXXX, KC_GRV,  KC_ESC,  KC_TAB,   KC_BSPC,        KC_DEL,  KC_RALT, KC_LEFT, KC_RGHT, XXXXXXX },
+  {XXXXXXX, KC_GRV,  KC_ESC,  KC_TAB,   KC_BSPC,        KC_DEL,  KC_LALT, KC_LEFT, KC_RGHT, XXXXXXX },
   {XXXXXXX, KC_QUOT, KC_LGUI, DUAL_L,   KC_LCTL,        KC_LSFT, DUAL_R,  KC_UP,   KC_DOWN, XXXXXXX }
 },
 
@@ -74,9 +74,9 @@ void matrix_scan_user(void) {
     uint8_t layer = biton32(layer_state);
     
     switch (layer) {
-    	case _BL:
-    		set_led_off;
-    		break;
+      case _BL:
+        set_led_off;
+        break;
         case _RAISE:
             set_led_blue;
             break;
