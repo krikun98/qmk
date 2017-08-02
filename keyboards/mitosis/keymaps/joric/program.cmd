c2aa58931c9990dddddaa386ed414e335fb000b9
joric <joric@users.noreply.github.com>
2017-08-02 12:04:15 +0500

    updated program.cmd

--- keyboards/mitosis/keymaps/joric/program.cmd
+++ keyboards/mitosis/keymaps/joric/program.cmd
@@ -1,18 +1,19 @@
 @echo off
-set conf=joric
 
-bash -c "cd ../../../../ && make mitosis-%conf%"
-IF errorlevel 1 exit
-
-set file=..\..\..\..\mitosis_%conf%.hex
-
-set port=COM9
-echo Waiting for %port% (press reset)...
-:loop1
-MODE %port% | grep "RTS"
-IF errorlevel 1 goto loop1
+for %%f in (%~dp0.) do set layout=%%~nxf
 
 set path=C:\WinAVR-20100110\bin;%path%
+set port=COM9
+set keyboard=mitosis
+set file=../../../../%keyboard%_%layout%.hex
+
+bash -c "cd ../../../../ && make %keyboard%-%layout%" || exit
+
+echo Waiting for %port% (press reset)...
+
+:while1
+MODE %port% | grep "RTS"
+IF errorlevel 1 goto while1
 
 avrdude -p atmega32u4 -P %port%  -cavr109  -b57600 -U flash:w:"%file%":i
 
