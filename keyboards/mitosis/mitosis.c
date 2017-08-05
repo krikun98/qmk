7c913c3a183b0951cb5e163d2e25ee06c2d75909
joric <joric@users.noreply.github.com>
2017-08-05 19:32:43 +0500

    add LED init sequence on mitosis power up

--- keyboards/mitosis/mitosis.c
+++ keyboards/mitosis/mitosis.c
@@ -18,6 +18,10 @@ void matrix_init_kb(void) {
 	matrix_init_user();
 	uart_init();
 	led_init();
+	set_led_red; _delay_ms(250);
+	set_led_blue; _delay_ms(250);
+	set_led_green; _delay_ms(250);
+	set_led_off;
 }
 
 void matrix_scan_kb(void) {
