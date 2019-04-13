35e9d48f973c43d15a8abfda08704cb3c2eb4b9b
joric <joric@users.noreply.github.com>
2019-04-13 23:50:39 +0500

    fixed DIODE_DIRECTION to COL2ROW after recent update

--- keyboards/jian_ble/config.h
+++ keyboards/jian_ble/config.h
@@ -73,7 +73,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
-#define DIODE_DIRECTION ROW2COL
+#define DIODE_DIRECTION COL2ROW
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
