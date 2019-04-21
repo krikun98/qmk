0fad7499a810945f17389d54bd2b2b1916c0d8d5
joric <joric@users.noreply.github.com>
2019-04-22 03:09:44 +0500

    added rgblight files

---
+++ keyboards/mitosis_ble/rgblight.h
@@ -0,0 +1,34 @@
+#ifndef RGBLIGHT_H
+#define RGBLIGHT_H
+
+#include "neopixel.h"
+#include "neopixel.c"
+#include "quantum.h"
+
+neopixel_strip_t m_strip;
+uint8_t dig_pin_num = LED_DATA_PIN;
+uint8_t leds_per_strip = 22;
+
+void rgblight_mode(int mode) {
+    neopixel_init(&m_strip, dig_pin_num, leds_per_strip);
+    neopixel_clear(&m_strip);
+
+    uint8_t r,g,b,a=255;
+
+    for (int j=0; j<4; j++) {
+
+        switch(j) {
+            case 0: r=a,g=0,b=0; break;
+            case 1: r=0,g=a,b=0; break;
+            case 2: r=0,g=0,b=a; break;
+            case 3: r=0,g=0,b=0; break;
+        }
+
+        for (int i=0; i<leds_per_strip; i++) {
+            neopixel_set_color_and_show(&m_strip, i, r, g, b);
+            nrf_delay_ms(25);
+        }
+    }
+}
+
+#endif //RGBLIGHT_H
