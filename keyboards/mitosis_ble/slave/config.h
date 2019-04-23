5cdd61dde376826cf5ba8e94b718d76622c1d22a
joric <joric@users.noreply.github.com>
2019-04-23 07:07:27 +0500

    layout moved to pin_assign.h

--- keyboards/mitosis_ble/slave/config.h
+++ keyboards/mitosis_ble/slave/config.h
@@ -3,15 +3,6 @@
 
 #include "custom_board.h"
 
-#define THIS_DEVICE_ROWS 1
-#define THIS_DEVICE_COLS 22
-
-#define MATRIX_ROW_PINS { NO_PIN }
-
-#define MATRIX_COL_PINS { S01, S02, S03, S04, S05, S06, S07, S08, S09, S10, \
-                          S11, S12, S13, S14, S15, S16, S17, S18, S19, S20, \
-                          S21, S22 }
-
 #define IS_LEFT_HAND  false
 
 #endif /* KEYBOARDS_NRF52_SEPARATE_TEST_ */
