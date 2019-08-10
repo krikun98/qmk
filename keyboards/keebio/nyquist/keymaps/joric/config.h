8c31c4faac07f29a6b515293e4bbaf8341525303
joric <joric@users.noreply.github.com>
2019-08-10 20:06:11 +0500

    added RGB leds

--- keyboards/keebio/nyquist/keymaps/joric/config.h
+++ keyboards/keebio/nyquist/keymaps/joric/config.h
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// #define USE_I2C
+//#define USE_I2C
 
 /* Select hand configuration */
 // #define MASTER_RIGHT
@@ -29,3 +29,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+#define RGBLED_SPLIT { 6, 6 }
