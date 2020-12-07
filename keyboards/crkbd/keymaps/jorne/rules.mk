1f353772f22775ce02a35f08963202975a09ee50
krikun98 <krikun98@gmail.com>
2020-12-07 16:02:03 +0300

    qmk dfu

--- keyboards/crkbd/keymaps/jorne/rules.mk
+++ keyboards/crkbd/keymaps/jorne/rules.mk
@@ -17,7 +17,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = yes     # Enable one-hand typing
 
-BOOTLOADER = caterina
+BOOTLOADER = qmk-dfu
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
